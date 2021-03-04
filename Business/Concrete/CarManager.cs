877e56ac04828fce87d5871a84721b1fadd9eabf
Eser <Eser@LAPTOP-EVSNA4JH>
2021-03-04 06:02:09 +0300

    ef

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -10,15 +10,40 @@ namespace Business.Concrete
     public class CarManager : ICarService
     {
         ICarDal _carDal;
+        
 
         public CarManager(ICarDal carDal)
         {
             _carDal = carDal;
+            
+        }
+
+        public void Add(Car car)
+        {
+            if (car.CarName.Length>=2 && car.DailyPrice>0)
+            {
+                _carDal.Add(car);
+                Console.WriteLine(car.CarName+""+"isimli araç veritabanına eklendi..");
+            }
+            else
+            {
+                Console.WriteLine("HATALI EKLEME!--Araç ismi en az 2 karakter ve aracın günlük ücreti 0'dan büyük olmalıdır.. ");
+            }
         }
 
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.Id == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
         }
     }
 }
