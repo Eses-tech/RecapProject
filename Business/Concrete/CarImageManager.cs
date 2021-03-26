99db81b83616856687a4a7e811b1d41fb286a8df
Eser <Eser@LAPTOP-EVSNA4JH>
2021-03-26 04:37:01 +0300

    13.gün

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        public IResult Add(CarImage entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Delete(CarImage entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<CarImage> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(CarImage entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
